4a70cc96356e6ae9f7734c5fd4c4df379e3a6390
Joshua Fielding <josh.e.fielding@gmail.com>
2018-12-11 09:01:38 +1100

    11/12 morning styling

--- js/shapes.js
+++ js/shapes.js
@@ -102,11 +102,6 @@ heart.forEach((heart, index) => {
 })
 
 
-
-
-
-
-
 // triangle-bottom-right here
 const triangle_bottom_right = document.querySelectorAll('.triangle-bottom-right')
 
@@ -126,6 +121,35 @@ triangle_bottom_right.forEach((triangle_bottom_right, index) => {
       {transform: 'rotate(0deg)'}
     ],
 
+    {
+      // here we use the index to create a staggered animation delay
+      delay: 300 * index,
+      duration: 5000,
+      iterations: Infinity
+    }
+  )
+})
+
+
+
+
+// squiggles here
+const squiggles = document.querySelectorAll('.squiggle')
+
+squiggles.forEach((squiggle, index) => {
+  // gets a random number between 0 and 45 using our random function
+  // from stackoverflow
+  const randomNumber = random(0, 45)
+  // create a bit of randomness for our animation delay
+  console.log(randomNumber)
+  squiggle.animate(
+    [
+      {transform: 'rotate(0deg)'},
+      // here we join or random number into our rotate property
+      // { transform: 'rotate(' + randomNumber + 'deg)' },
+      {transform: `rotate(${randomNumber}deg)`},
+      {transform: 'rotate(0deg)'}
+    ],
     {
       // here we use the index to create a staggered animation delay
       delay: 300 * index,
