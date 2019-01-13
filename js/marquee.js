1c6e9d24a44f2072b1914cd29453b9f65a65979c
Your Name <josh.e.fielding@gmail.com>
2019-01-13 15:07:41 +1100

    Update marquee.js

--- js/marquee.js
+++ js/marquee.js
@@ -1,26 +1,21 @@
 // ==== marquee effect goes here ====
 
-function makeMarquee(list) {
-  // this is now taking a list of things (see below)
-  // pick the marquees in the code
-  // slightly diff than before due to querySelectorAll as 
-  // this is a list of ALL marquees, not just the first one
-  const marquees = document.querySelectorAll('.marquee span')
-  
-  // for each thing in the array below, loop over it
-  list.forEach((title, index) => {
-    // fill up with the individual text (e.g. FIFTY, then Rik, then SuperHi)
-    const marqueeText = new Array(50).fill(title).join(' — ')
-    
-    // pick the 1st, then 2nd, ... version of the marquees from above
-    const marquee = marquees[index]
-    
-    // add the text!
-    marquee.innerHTML = marqueeText
-  })
+function makeMarquee() {
+  const title = 'NEW Tournament coming soon'
+  // an array is a list of things
+  // ['lawrence', 'rik', 'milan', 'ryan', 'adam', 'krista']
+  // here we make a new empty array with 50 spaces in it
+  // then we fill it with the text from our title (50 times)
+  // we then join them all together as one text string using a ' — '
+  const marqueeText = new Array(50).fill(title).join(' — ')
+  // querySelector and querySelectorAll are the same as $ in jQuery
+  const marquee = document.querySelector('.marquee span')
+  // using innerHTML lets us set the content inside an element
+  marquee.innerHTML = marqueeText
 }
-// here we actually run our makeMarquee function, but now passing it
-// an array of things to fill up each marquee
-makeMarquee([
-  'NEW Tournament Coming Soon!',
-])
+
+// here we actually run our makeMarquee function
+makeMarquee()
+
+
+
